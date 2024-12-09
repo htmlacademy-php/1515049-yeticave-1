5644e4a2614276e2604bfdab5fa3ac3083718fbf
Наталья Романова <Natalocka_ne@mail.ru>
2024-12-09 23:30:21 +0300

    Ренейминг под стандарты

--- index.php
+++ index.php
@@ -1,48 +1,60 @@
 <?php
-$is_auth = rand(0, 1);
 
-$user_name = 'Наталья'; // укажите здесь ваше имя
+/**
+ * Форматирует цену лота
+ * @param int|float $price
+ * @return string
+ */
+function formatPrice(int|float $price): string
+{
+    $price = number_format($price, 0, '.', ' ');
+    return $price . ' ₽';
+}
+
+$isAuth = rand(0, 1);
+
+$userName = 'Наталья'; // укажите здесь ваше имя
 
 $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
 
-$ads = [
-    ["title" => "2014 Rossignol District Snowboard",
+$lots = [
+    [
+        "title" => "2014 Rossignol District Snowboard",
         "category" => $categories[0],
         "price" => 10999,
         "img_url" => "img/lot-1.jpg",
     ],
-    ["title" => "DC Ply Mens 2016/2017 Snowboard",
+    [
+        "title" => "DC Ply Mens 2016/2017 Snowboard",
         "category" => $categories[0],
         "price" => 159999,
         "img_url" => "img/lot-2.jpg",
     ],
-    ["title" => "Крепления Union Contact Pro 2015 года размер L/XL",
+    [
+        "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
         "category" => $categories[1],
         "price" => 8000,
         "img_url" => "img/lot-3.jpg",
     ],
-    ["title" => "Ботинки для сноуборда DC Mutiny Charocal",
+    [
+        "title" => "Ботинки для сноуборда DC Mutiny Charocal",
         "category" => $categories[2],
         "price" => 10999,
         "img_url" => "img/lot-4.jpg",
     ],
-    ["title" => "Куртка для сноуборда DC Mutiny Charocal",
+    [
+        "title" => "Куртка для сноуборда DC Mutiny Charocal",
         "category" => $categories[3],
         "price" => 7500,
         "img_url" => "img/lot-5.jpg",
     ],
-    ["title" => "Маска Oakley Canopy",
+    [
+        "title" => "Маска Oakley Canopy",
         "category" => $categories[5],
         "price" => 5400,
         "img_url" => "img/lot-6.jpg",
     ],
 ];
-function format_price(int $price): string
-{
-    $result = ceil($price);
-    $result = number_format($result, 0, '.', ' ');
-    return $result . ' ₽';
-}
 
 ?>
 <!DOCTYPE html>
@@ -71,13 +83,15 @@ function format_price(int $price): string
             <nav class="user-menu">
 
                 <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-                <?php if ($is_auth === 1): ?>
+                <?php
+                if ($isAuth === 1): ?>
                     <div class="user-menu__logged">
-                        <p><?= htmlspecialchars($user_name, ENT_QUOTES, 'UTF-8'); ?></p>
+                        <p><?= htmlspecialchars($userName, ENT_QUOTES, 'UTF-8'); ?></p>
                         <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                         <a class="user-menu__logout" href="#">Выход</a>
                     </div>
-                <?php else: ?>
+                <?php
+                else: ?>
                     <ul class="user-menu__list">
                         <li class="user-menu__item">
                             <a href="#">Регистрация</a>
@@ -87,7 +101,8 @@ function format_price(int $price): string
                         </li>
                     </ul>
 
-                <?php endif; ?>
+                <?php
+                endif; ?>
 
             </nav>
         </div>
@@ -100,11 +115,13 @@ function format_price(int $price): string
                 горнолыжное снаряжение.</p>
             <ul class="promo__list">
                 <!--заполните этот список из массива категорий-->
-                <?php foreach ($categories as $category): ?>
+                <?php
+                foreach ($categories as $category): ?>
                     <li class="promo__item promo__item--boards">
                         <a class="promo__link" href="pages/all-lots.html"><?= $category ?>></a>
                     </li>
-                <?php endforeach; ?>
+                <?php
+                endforeach; ?>
             </ul>
         </section>
         <section class="lots">
@@ -113,19 +130,20 @@ function format_price(int $price): string
             </div>
             <ul class="lots__list">
                 <!--заполните этот список из массива с товарами-->
-                <?php foreach ($ads as $ad): ?>
+                <?php
+                foreach ($lots as $lot): ?>
                     <li class="lots__item lot">
                         <div class="lot__image">
-                            <img src="<?= $ad["img_url"] ?>" width="350" height="260" alt="<?= $ad["title"] ?>">
+                            <img src="<?= $lot["img_url"] ?>" width="350" height="260" alt="<?= $lot["title"] ?>">
                         </div>
                         <div class="lot__info">
-                            <span class="lot__category"><?= $ad["category"] ?></span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $ad["title"] ?></a>
+                            <span class="lot__category"><?= $lot["category"] ?></span>
+                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lot["title"] ?></a>
                             </h3>
                             <div class="lot__state">
                                 <div class="lot__rate">
                                     <span class="lot__amount">Начальная цена</span>
-                                    <span class="lot__cost"><?= format_price($ad["price"]) ?></span>
+                                    <span class="lot__cost"><?= formatPrice($lot["price"]) ?></span>
                                 </div>
                                 <div class="lot__timer timer">
                                     12:23
@@ -133,7 +151,8 @@ function format_price(int $price): string
                             </div>
                         </div>
                     </li>
-                <?php endforeach; ?>
+                <?php
+                endforeach; ?>
             </ul>
         </section>
     </main>
@@ -143,11 +162,13 @@ function format_price(int $price): string
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($categories as $category): ?>
+            <?php
+            foreach ($categories as $category): ?>
                 <li class="nav__item">
                     <a href="pages/all-lots.html"><?= $category ?></a>
                 </li>
-            <?php endforeach; ?>
+            <?php
+            endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
