be8d150a4d21815e26112c86e870d814a37d5a9e
Наталья Романова <Natalocka_ne@mail.ru>
2024-12-13 20:50:42 +0300

    Правка нейминга

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 require_once 'helpers.php';
 require_once 'data.php';
 
-$page_content = include_template('main.php', [
+$pageContent = include_template('main.php', [
     'categories' => $categories,
     'lots' => $lots,
 ]);
 
-$layout_content = include_template('layout.php', [
-    'content' => $page_content,
+$layoutContent = include_template('layout.php', [
+    'content' => $pageContent,
     'title' => "Yeti Cave - Главная",
     'isAuth' => $isAuth,
     'userName' => $userName,
     'categories' => $categories,
 ]);
 
-print($layout_content);
+print($layoutContent);
